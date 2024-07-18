455cb4ad0b9c679a3d1c07e86915e02b21bab7d0
darren464 <135610898+nebo2002@users.noreply.github.com>
2024-07-18 21:13:23 +0530

    Create Functions & Methods.java

---
+++ Functions & Methods.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.lang.*;
+
+class HelloWorld {
+    
+    public static void printName(String name)
+    {
+        System.out.print("The winner is "+name);
+        
+    }
+      public static int multiply(int a,int b)
+    {
+        int total=a*b;
+        return total;
+    }
+    
+    public static int calculate(int a,int b)
+    {
+        int total=a+b;
+        return total;
+    }
+    public static int factorial(int a)
+    {
+        int total=1;
+        for(int i=a;i>=1;i--)
+        {
+            total=total*i;
+        }
+        return total;
+    }
+    
+    public static void main(String[] args) 
+    {
+        
+        Scanner scanner=new Scanner(System.in);
+        
+        String name=scanner.nextLine();
+        
+        printName(name);
+        
+        int a=scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        // System.out.print("The sum is "+calculate(a,b));
+         System.out.print("The product is "+multiply(a,b));
+         System.out.print("The factorial is "+factorial(a));
+        
+    }
+}
