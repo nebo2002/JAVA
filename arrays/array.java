cea12c30706622912f77cb2712fb7eb4268bf024
darren464 <135610898+nebo2002@users.noreply.github.com>
2024-07-20 22:07:27 +0530

    Create array.java

---
+++ arrays/array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+
+class HelloWorld {
+    
+    public static int SearchElement(int arr[],int n,int x)
+    {
+        for (int i=0;i<n;i++)
+        {
+            if (arr[i]==x)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner=new Scanner(System.in);
+    
+        int n =scanner.nextInt();
+        int arr[]=new int[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scanner.nextInt();
+        }
+        
+        System.out.print("Enter x:");
+        int x=scanner.nextInt();
+        System.out.print(SearchElement(arr,n, x));
+     
+     
+    }
+}
